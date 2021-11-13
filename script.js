90bb837e2d3b6fa5a7977d5d6be4741ba3b3735c
huzitaiwo <taiwohusayn5@gmail.com>
2021-11-13 14:45:53 +0100

    added js navaction

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const navigationLinks = document.querySelectorAll('.nav-links');
-const circle = document.querySelectorAll('.nav-links a');
-// console.log(circle[1]); 
+const navigation = document.querySelector('nav');
+const navigationLinks = document.querySelector('.nav-links');
+const burgerMenu = document.querySelector('.hamburger');
+const burgerMenuIcon = document.querySelector('.hamburger img');
+
+burgerMenu.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    navigationLinks.classList.toggle('active');
+
+    //   toggle burgerMenu and close icon
+  if (navigationLinks.classList.contains("active")) {
+    burgerMenuIcon.src = "./icons/close.svg";
+  } else {
+    burgerMenuIcon.src = "./icons/hamburger.svg";
+  }
+})
